580914ac161f04ed99ee1616d7913c9ce8a85c32
andresgc04 <andresgc1997@outlook.com>
2022-07-27 17:07:00 -0400

    Agregar Leccion02 02-03-Uso-Basico-Var-Let-Const en el curso de Universidad_JavaScript_Cero_Experto_JavaScript

---
+++ Universidad_JavaScript_Cero_Expero_JavaScript/leccion02/02-03-Uso-Basico-Var-Let-Const.js
@@ -0,0 +1,21 @@
+let nombre;
+nombre = "Andres";
+console.log(nombre);
+
+
+const apellido = "Guerrero";
+//apellido = "Cordero"; //Apellido = "Guerrero"; Error no se puede cambiar el valor de una constante
+
+
+
+//Buenas practicas al declarar nombres de variables:
+//Ejemplos:
+let nombreCompleto = "Andres Eugenio Guerrero Cordero";
+console.log(nombreCompleto);
+
+let x, y;
+x = 10, y =20;
+
+let z = x + y;
+
+console.log(z);
