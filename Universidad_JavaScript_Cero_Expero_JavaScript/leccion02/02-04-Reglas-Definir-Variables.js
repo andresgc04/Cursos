5ac882358f4e6a889d20936d5b0a2b12fa5d587f
andresgc04 <andresgc1997@outlook.com>
2022-07-27 17:16:37 -0400

    Agregar Leccion02 02-04-Reglas-Definir-Variables del curso Universidad-JavaScript-Cero-Experto

---
+++ Universidad_JavaScript_Cero_Expero_JavaScript/leccion02/02-04-Reglas-Definir-Variables.js
@@ -0,0 +1,22 @@
+let nombreCompleto = "Andres Eugenio Guerrero Cordero";
+let nombrecompleto = "Carlos Lara";
+
+console.log(nombreCompleto);
+console.log(nombrecompleto);
+
+/*
+Reglas para declarar el nombre de una variable:
+
+1) No se puede declarar una variable comenzando el nombre con numeros.
+
+2) Los caracteres permitidos son letras en minusculas y mayusculas, como tambien son permitidos los caracteres especiales como: _ o $.
+
+casos que son correctos para declarar variables son los siguientes ejemplos:
+
+let nombreCompleto;
+let _nombreCompleto;
+let $nombreCompleto;
+
+3) No se pueden utilizar los nombres de las palabras reservadas.
+*/
+
