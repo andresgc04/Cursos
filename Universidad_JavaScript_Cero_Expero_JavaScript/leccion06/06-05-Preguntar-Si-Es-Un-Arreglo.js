54533598622c0362bc6ce40584ffa993210f9785
andresgc04 <andresgc1997@outlook.com>
2022-10-04 00:03:46 -0400

    Agregando Leccion-06-05-Preguntar-Si-Es-Un-Arreglo en el Curso Universidad de JavaScript

---
+++ Universidad_JavaScript_Cero_Expero_JavaScript/leccion06/06-05-Preguntar-Si-Es-Un-Arreglo.js
@@ -0,0 +1,6 @@
+//Cómo preguntar en JavaScript si estamos trabajando con un arreglo:
+const autos = ['BMW', 'Mercedes Benz', 'Volvo'];
+
+console.log(Array.isArray(autos));
+
+console.log(autos instanceof Array);
