5abf7b7c49887502b310c6ba01cb83d60004f5b3
andresgc04 <andresgc1997@outlook.com>
2022-10-03 23:00:06 -0400

    Agregando Leccion06, Leccion-06-01-Arreglos y Leccion-06-02-Recorrer-Elementos-Arreglos del Curso De La Universidad De JavaScript

---
+++ Universidad_JavaScript_Cero_Expero_JavaScript/leccion06/06-01-Arreglos.js
@@ -0,0 +1,8 @@
+//Sintaxis antigua de como crear arreglos en JavaScript:
+
+//let autos = new Array('BMW','Mercedes Benz', 'Volvo');
+
+//Sintaxis nueva y recomendable para crear arreglos en JavaScript:
+const autos = ['BMW', 'Mercedes Benz', 'Volvo'];
+
+console.log(autos);
---
+++ Universidad_JavaScript_Cero_Expero_JavaScript/leccion06/06-02-Recorrer-Elementos-Arreglos.js
@@ -0,0 +1,9 @@
+const autos = ["BMW", "Mercedes Benz", "Volvo"];
+
+//Recorrer elementos de un arreglo por su indice:
+console.log(autos[0]);
+console.log(autos[2]);
+
+for (let i = 0; i < autos.length; i++) {
+  console.log(autos[i]);
+}
