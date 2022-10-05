f7c909ed464ab38d195a3ef88b300e1bee80fbbf
andresgc04 <andresgc1997@outlook.com>
2022-10-05 00:24:00 -0400

    Agregando leccion07-03-Funciones-Tipos-Expresiones del Curso Universidad JavaScript

---
+++ Universidad_JavaScript_Cero_Expero_JavaScript/leccion07/leccion07-03-Funciones-Tipos-Expresiones.js
@@ -0,0 +1,6 @@
+//Declaración de Función De Tipo Expresión:
+let sumar = function (a,b){return a+b};
+
+//Llamando a la Función De Tipo Expresión:
+let resultado = sumar(1,2);
+console.log(resultado);
