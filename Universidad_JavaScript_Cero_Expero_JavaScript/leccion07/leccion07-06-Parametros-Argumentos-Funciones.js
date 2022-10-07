67e43f01f47e6c864263e5e4c9d6de00fa38823c
andresgc04 <andresgc1997@outlook.com>
2022-10-07 00:51:59 -0400

    Agregar Leccion07-06-Parametros-Argumentos-Funciones Del Curso Universidad JavaScript

---
+++ Universidad_JavaScript_Cero_Expero_JavaScript/leccion07/leccion07-06-Parametros-Argumentos-Funciones.js
@@ -0,0 +1,11 @@
+//Declaración Función De Tipo Expresión:
+let sumar = function (a = 4, b = 5) {
+  console.log(arguments[0]);
+  console.log(arguments[1]);
+  console.log(arguments[2]);
+
+  return a + b + arguments[2];
+};
+
+let resultado = sumar(3, 6, 7);
+console.log(resultado);
