8f5cca6d3ca707bc8e99c1a8252dbf475ac6696c
andresgc04 <andresgc1997@outlook.com>
2022-10-08 13:03:49 -0400

    Agregando Leccion08, Leccion08-01-Objetos

---
+++ Universidad_JavaScript_Cero_Expero_JavaScript/leccion08/leccion08-01-Objetos.js
@@ -0,0 +1,16 @@
+let x = 10; //Literal de tipo primitivo.
+console.log(x.length);
+
+//Creando un objeto:
+let persona = {
+  nombre: "Andres",
+  apellido: "Guerrero",
+  email: "aguerrero@outlook.com",
+  edad: 25,
+};
+
+console.log(persona.nombre);
+console.log(persona.apellido);
+console.log(persona.edad);
+
+console.log(persona);
