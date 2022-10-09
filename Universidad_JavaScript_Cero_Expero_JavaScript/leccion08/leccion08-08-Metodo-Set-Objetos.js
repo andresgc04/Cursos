a108e129e52872d4f8e0d95ce6a2333292e1c507
andresgc04 <andresgc1997@outlook.com>
2022-10-09 15:11:11 -0400

    Agregando Leccion08-08-Metodo-Set-Objetos Del Curso Universidad JavaScript

---
+++ Universidad_JavaScript_Cero_Expero_JavaScript/leccion08/leccion08-08-Metodo-Set-Objetos.js
@@ -0,0 +1,23 @@
+let persona = {
+  nombre: "Andres",
+  apellido: "Guerrero",
+  email: "andresguerrero@outlook.com",
+  edad: 25,
+  idioma: "es",
+  get lang() {
+    return this.idioma.toUpperCase();
+  },
+  set lang(lang) {
+    this.idioma = lang.toUpperCase();
+  },
+  get nombreCompleto() {
+    return this.nombre + " " + this.apellido;
+  },
+};
+
+console.log(persona.lang);
+
+persona.lang = "en";
+
+console.log(persona.lang);
+console.log(persona.idioma);
