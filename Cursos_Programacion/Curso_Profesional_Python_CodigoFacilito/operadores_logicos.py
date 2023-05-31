a714da7acfd21ba737411f87469b8c3996d35b54
andresgc04 <andresgc1997@outlook.com>
2023-05-31 14:35:17 -0400

    Agregando Modulo De Operadores Logicos
    Agregando modulo de operadores logicos en el curso profesional de python

---
+++ Cursos_Programacion/Curso_Profesional_Python_CodigoFacilito/operadores_logicos.py
@@ -0,0 +1,20 @@
+"""
+Existen tres tipos de operadores logicos que son los siguientes:
+
+and (&&), or (||) y not (!)
+"""
+
+#and:
+resultado_final = True and True and 100 > 20
+
+print(resultado_final)
+
+#or:
+resultado_final2 = False or False or 100 > 20
+
+print(resultado_final2)
+
+#not:
+resultado_final3 = not False
+
+print(resultado_final3)
