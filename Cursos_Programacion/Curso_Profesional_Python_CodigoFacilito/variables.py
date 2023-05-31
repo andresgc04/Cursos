6ad1bd98fe8401fe3abd3cf080da8b0cd868f747
andresgc04 <andresgc1997@outlook.com>
2023-05-31 10:29:02 -0400

    Creando Carpeta Del Curso Profesional Python
    Creando Modulo De Variables

---
+++ Cursos_Programacion/Curso_Profesional_Python_CodigoFacilito/variables.py
@@ -0,0 +1,11 @@
+#Nomenclatura de las variables:
+
+#<nombre> = <valor_almacenar>
+
+titulo_curso = 'Curso Profesional De Python'
+
+print(titulo_curso)
+
+nombre_completo = 'Andrés Eugenio Guerrero Cordero'
+
+print(nombre_completo)
