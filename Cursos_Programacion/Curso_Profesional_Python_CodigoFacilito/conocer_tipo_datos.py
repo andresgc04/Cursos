b16a9beb39b7dc51c0e6019908914cc259444380
andresgc04 <andresgc1997@outlook.com>
2023-05-31 14:41:59 -0400

    Agregando Modulo De Conocer Tipos De Datos
    Agregando modulo de conocer tipo de datos en el curso profesional de python

---
+++ Cursos_Programacion/Curso_Profesional_Python_CodigoFacilito/conocer_tipo_datos.py
@@ -0,0 +1,23 @@
+valor = 'Eduardo'
+
+tipo_valor = type(valor)
+
+print(tipo_valor)
+
+valor = 2
+
+tipo_valor2 = type(valor)
+
+print(tipo_valor2)
+
+valor = 3.1
+
+tipo_valor3 = type(valor)
+
+print(tipo_valor3)
+
+valor = True
+
+tipo_valor4 = type(valor)
+
+print(tipo_valor4)
