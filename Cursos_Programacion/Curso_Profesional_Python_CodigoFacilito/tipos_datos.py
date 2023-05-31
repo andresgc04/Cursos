ddc38781b3b171efa463e26acf86e25caf1c7e54
andresgc04 <andresgc1997@outlook.com>
2023-05-31 12:05:31 -0400

    Agregando Modulo De Tipos De Datos
    Agregando modulo de tipos de datos en el curso profesional de python

---
+++ Cursos_Programacion/Curso_Profesional_Python_CodigoFacilito/tipos_datos.py
@@ -0,0 +1,45 @@
+"""
+Existen diferentes tipos de datos que son los siguientes:
+
+String,
+Int,
+Bool
+"""
+
+# String
+
+titulo_curso = 'Curso Profesional De Python'
+
+print(titulo_curso)
+
+nombre_completo = 'Andrés Eugenio Guerrero Cordero'
+
+print(nombre_completo)
+
+mensaje = "'Codigo Facilito'"
+
+print(mensaje)
+
+mensaje2 = '''Te encuentras
+en el curso: Profesional de Python.
+En CódigoFacilito.'''
+
+print(mensaje2)
+
+#Int
+#Al dividir con / Obtenemos un tipo de dato flotante.
+#Al dividir con // Obtenemos un tipo de dato entero.
+
+numero_uno = 10 + 20
+
+print(numero_uno)
+
+#Float
+
+numero_dos = 3.14
+
+print(numero_dos)
+
+#Bool
+valor = True
+print(valor)
