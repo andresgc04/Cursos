bed3b2a35e3b03a260dc6490b04e5683ee0bc8cd
andresgc04 <andresgc1997@outlook.com>
2023-05-31 12:53:16 -0400

    Agregando Modulo De Operadores Relacionales
    Agregando modulo de operadores relacionales en el curso profesional de python

---
+++ Cursos_Programacion/Curso_Profesional_Python_CodigoFacilito/operadores_relacionales.py
@@ -0,0 +1,22 @@
+numero_uno = 20
+numero_dos = 50
+
+"""
+Los operadores relacionales son 6 y son los siguientes:
+
+Mayor que (>)
+
+Menor que (<)
+
+Mayor e igual que (>=)
+
+Menor e igual que (<=)
+
+igual que (==)
+
+Negación (!=)
+"""
+
+resultado = numero_uno != numero_dos
+
+print(resultado)
