773891a27e3645bdbdfd32d79be40c1f1b1420f1
andresgc04 <andresgc1997@outlook.com>
2023-05-31 15:59:35 -0400

    Agregando Clase De Actualizar Elementos En Listas
    Agregando Clase De Actualizar Elementos En Listas del modulo_2 del curso profesional python

---
+++ Cursos_Programacion/Curso_Profesional_Python_CodigoFacilito/modulo_2/listas.py
@@ -0,0 +1,14 @@
+#Las listas contienen indices que son los que manejan las posiciones de sus elementos.
+#                    0        1         2        3      4
+#indices con numeros negativos se leen de izquierda a derecha y iniciando desde el -1
+#                   -5        -4       -3       -2      -1
+listas_cursos = ['Python', 'Django', 'Flask', 'Ruby', 'Java']
+
+primer_curso = listas_cursos[0]
+print(primer_curso)
+
+segundo_curso = listas_cursos[1]
+print(segundo_curso)
+
+ultimo_curso = listas_cursos[4]
+print(ultimo_curso)
