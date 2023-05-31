840b2eb13514562dc4b38c495b506d7c9e80643a
andresgc04 <andresgc1997@outlook.com>
2023-05-31 15:21:05 -0400

    Agregando Modulo De Convetir Tipos De Datos
    Agregando modulo de convertir tipos de datos en el curso profesional de python

---
+++ Cursos_Programacion/Curso_Profesional_Python_CodigoFacilito/convertir_tipos_datos.py
@@ -0,0 +1,15 @@
+nombre_completo = input('Ingresa tu nombre completo: ') #str
+
+edad = int(input('Ingresa tu edad: '))
+
+print(edad)
+print(type(edad))
+
+altura = float(input('Ingresa tu altura: '))
+
+print(altura)
+print(type(altura))
+
+autorizacion = input('¿Autorizas el programa? (si/no)') == 'si'
+
+print(autorizacion)
