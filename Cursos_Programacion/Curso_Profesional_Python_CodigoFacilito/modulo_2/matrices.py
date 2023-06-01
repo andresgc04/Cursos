2d4a1e9b7d1549a1498d6a399667c8a6370fb642
andresgc04 <andresgc1997@outlook.com>
2023-06-01 12:52:41 -0400

    Agregando Clase De Matrices
    Ágregando clase de matrices en el modulo_2 del curso profesional python.

---
+++ Cursos_Programacion/Curso_Profesional_Python_CodigoFacilito/modulo_2/matrices.py
@@ -0,0 +1,9 @@
+#Matriz bidimensional:
+
+columna_a = [10, 20, 30, 40]
+columna_b = [30, 40, 50, 60]
+
+matriz = [columna_a, columna_b] #Sera una matriz 2 x 2, la cual tendra 2 columnas y 4 filas.
+
+print(matriz)
+print(matriz[1][3])
