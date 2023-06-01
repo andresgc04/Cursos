9380ec84b9eeb97a0934cf0789af144e8ccf9812
andresgc04 <andresgc1997@outlook.com>
2023-06-01 16:54:42 -0400

    Agregando Clase Sobre Concatenar Textos (String)
    Agregando clase sobre concatenar textos (string) en el modulo_4 del curso profesional pyhon

---
+++ Cursos_Programacion/Curso_Profesional_Python_CodigoFacilito/modulo_4/concatenar_parte_1.py
@@ -0,0 +1,8 @@
+nombre = 'Eduardo Ismael'
+apellido = 'García'
+
+#nombre_completo = 'Mr.' + nombre + ' ' + apellido + '.'
+
+nombre_completo = 'Mr. %s %s %s.' %(nombre, apellido, 'Pérez')
+
+print(nombre_completo)
