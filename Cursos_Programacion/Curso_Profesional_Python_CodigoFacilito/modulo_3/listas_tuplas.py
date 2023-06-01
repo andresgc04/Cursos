191e30151430593a9eee8bf94376182033930a6c
andresgc04 <andresgc1997@outlook.com>
2023-06-01 15:05:23 -0400

    Agregar Clase Listas y Tuplas
    Agregar Clase De Listas y Tuplas En El Modulo_3 Del Curso Profesional Python

---
+++ Cursos_Programacion/Curso_Profesional_Python_CodigoFacilito/modulo_3/listas_tuplas.py
@@ -0,0 +1,15 @@
+cursos = ['Python', 'Django', 'Flask']
+
+#Convertir listas a tuplas:
+cursos_tupla = tuple(cursos)
+
+print(cursos_tupla)
+print(type(cursos_tupla))
+
+#Convertir tuplas a listas:
+niveles = ('Básico', 'Intermedio', 'Avanzado')
+
+niveles_lista = list(niveles)
+
+print(niveles_lista)
+print(type(niveles_lista))
