17e35299b92e3d737bb5f5cc9e7f119e4dd00f44
andresgc04 <andresgc1997@outlook.com>
2023-06-01 13:07:22 -0400

    Creando Modulo_3 y Agregando Clase De Tuplas
    Agregando clase sobre tuplas en el modulo_3 del curso profesional python[D

---
+++ Cursos_Programacion/Curso_Profesional_Python_CodigoFacilito/modulo_3/main.py
@@ -0,0 +1,8 @@
+#Tuplas.
+
+#Las tuplas nos permiten almacenar distintos tipos de datos, la unica diferencia a la lista es que las tuplas son inmutables, es decir, no se pueden modificar.
+
+#indice     0       1    2     3       4          5
+tupla = ('String', 10, 15.4, True, [1, 2, 3], (4, 5, 6))
+
+print(tupla)
