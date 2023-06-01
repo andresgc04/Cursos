8785bdae422c5191158b212ce4cf9b0c47d6bafd
andresgc04 <andresgc1997@outlook.com>
2023-06-01 13:17:58 -0400

    Agregando Clase De Indices De Tuplas
    Agregando clase de indices de tuplas en el curso profesional python

---
+++ Cursos_Programacion/Curso_Profesional_Python_CodigoFacilito/modulo_3/indices_tuplas.py
@@ -0,0 +1,11 @@
+cursos = ('Python', 'Flask', 'Django', 'Rails', 'MongoDB')
+#indice       0        1         2         3        4
+
+primer_curso = cursos[1]
+print(primer_curso)
+
+ultimo_curso = cursos[-1]
+print(ultimo_curso)
+
+sub_tupla = cursos[:3]
+print(sub_tupla)
