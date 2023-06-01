23ac0802ae3ad863505b446a7c14589c729f6332
andresgc04 <andresgc1997@outlook.com>
2023-06-01 12:17:57 -0400

    Agregando Clase De Metodos De Listas
    Agregando clase de metodos de listas en el modulo_2 del curso profesional python

---
+++ Cursos_Programacion/Curso_Profesional_Python_CodigoFacilito/modulo_2/metodos_listas.py
@@ -0,0 +1,30 @@
+listas = [8, 90, 1, 5, 44, 132, 600, 3, 4]
+
+#Método o Función Sort, sirve para ordenar las listas de forma ascendente (Menor a Mayor):
+#listas.sort()
+
+#print(listas)
+
+#Método o Función Sort(reverse=True) para poder ordenar las listas de forma descendente (Mayor a Menor):
+#listas.sort(reverse=True)
+
+#print(listas)
+
+#listas.sort()
+
+#Para conocer el numero menor y mayor de una lista:
+#print(listas[0]) #Min
+#print(listas[-1]) #Max
+
+#Funciones min y max nos ayudan para saber cual es nuestro numero menor o mayor de nuestras listas.
+
+#print(min(listas))
+#print(max(listas))
+
+#Para poder saber si un numero o un elemento se encuentra en nuestra lista, utilizamos la función in:
+
+print(5 in listas)
+
+#Para poder saber si un numnero o un elemento no se encuentra en nuestras listas, utilizamos la función not in:
+
+print(8 not in listas)
