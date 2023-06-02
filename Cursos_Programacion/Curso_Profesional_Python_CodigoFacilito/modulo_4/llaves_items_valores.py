65d55030dfda8b55c72b2d965b802ce92ba9f965
andresgc04 <andresgc1997@outlook.com>
2023-06-02 10:44:52 -0400

    Agregando Clase Sobre LLaves, Items y Valores De Los Diccionarios
    Agregando clase sobre las llaves, items y valores de los diccionarios en el modulo_4 del curso profesional python

---
+++ Cursos_Programacion/Curso_Profesional_Python_CodigoFacilito/modulo_4/llaves_items_valores.py
@@ -0,0 +1,18 @@
+diccionario = {'a' : 1, 'b' : 2, 'c' : 3, 'd' : 4}
+
+#keys
+#values 
+#items
+
+#El método keys se utiliza para obtener las llaves de un diccionario.
+llaves = tuple(diccionario.keys())
+
+print(llaves)
+
+#El método vlaues se utiliza para obtener los valores almacenados dentro de un diccionario.
+valores = tuple(diccionario.values())
+print(valores)
+
+#El método items se utiliza para obtener los items o elementos de un diccionario.
+elementos = tuple(diccionario.items())
+print(elementos)
