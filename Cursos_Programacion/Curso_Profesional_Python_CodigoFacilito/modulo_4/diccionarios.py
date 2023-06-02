bae2ce25c667b77d6e1b32272d5a75ba3c861216
andresgc04 <andresgc1997@outlook.com>
2023-06-02 10:11:02 -0400

    Agregando Clase Sobre Diccionarios
    Agregando clase sobre diccionarios en el modulo_4 del curso profesional python

---
+++ Cursos_Programacion/Curso_Profesional_Python_CodigoFacilito/modulo_4/diccionarios.py
@@ -0,0 +1,16 @@
+'''
+elementos = {}
+
+#Crea la llave con su valor.
+elementos['nombre'] = 'Cody'
+
+elementos[(1, 2, 3)] = 'La llave es una tupla'
+
+#Actualiza el valor que la llave almacena.
+elementos['nombre'] = 'CódigoFacilito'
+'''
+
+elementos = {'a' : 1, 'b' : 2, 'c' : 3, 'a' : 4}
+
+print(elementos)
+print(len(elementos))
