0a24c678a61c9295ff0b42e61f42149cceb296da
andresgc04 <andresgc1997@outlook.com>
2023-06-02 11:04:46 -0400

    Agregando Clase Sobre Eliminar Elementos De Un Diccionario
    Agregando Clase sobre elemininar elementos de un diccionario en el modulo_5 del curso profesional python

---
+++ Cursos_Programacion/Curso_Profesional_Python_CodigoFacilito/modulo_4/eliminar_elementos.py
@@ -0,0 +1,15 @@
+diccionario = {'a' : 1, 'b' : 2, 'c' : 3, 'd' : 4 }
+
+print(len(diccionario))
+
+del diccionario['a']
+
+valor = diccionario.pop('b')
+
+diccionario.clear()
+
+print(valor)
+
+print(diccionario)
+
+print(len(diccionario ))
