5194d3cd2fd414a43cace5eaf0346c4676fb36f3
andresgc04 <andresgc1997@outlook.com>
2023-06-02 09:23:14 -0400

    Agregando Clase Sobre Validar Substrings
    Agregando clase sobre validar substrings en el modulo_4 del curso profesional python

---
+++ Cursos_Programacion/Curso_Profesional_Python_CodigoFacilito/modulo_4/validar_sub_strings.py
@@ -0,0 +1,12 @@
+titulo_curso = 'Curso profesional de Python, donde aprenderemos Python'
+
+#La función count nos permite contar cuantas veces existen un string dentro de otro, que nos ayuda a buscar palabras, letras o frases.
+'''
+contador = titulo_curso.count('x')
+
+print(contador)
+'''
+
+print('Python' in titulo_curso)
+
+print(titulo_curso.startswith('Curso'))
