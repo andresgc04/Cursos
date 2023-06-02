9d9d01aaa9e975bccd6f6e3644d9fa9aa810ea79
andresgc04 <andresgc1997@outlook.com>
2023-06-02 09:51:35 -0400

    Agregando Clase Sobre Justificar Textos
    Agregando clase sobre justificar textos en el modulo_4 del curso profesional python

---
+++ Cursos_Programacion/Curso_Profesional_Python_CodigoFacilito/modulo_4/justificar_texto.py
@@ -0,0 +1,17 @@
+mensaje = 'Hola Mundo'
+
+#ljust -> Permite justificar a la izquierda.
+
+#mensaje = mensaje.ljust(20)
+
+#print(mensaje, '.')
+
+#rjust -> Permite justificar a la derecha.
+
+#mensaje = mensaje.rjust(20)
+
+#center -> Permite centrar nuestros textos.
+
+mensaje = mensaje.center(20)
+
+print(mensaje)
